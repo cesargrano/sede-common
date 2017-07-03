1cf13d6b85b3ce977bf6faa0d781fe14192c31fb
cesargrano <cesar@sedeinfo.com.br>
2017-07-03 10:49:40 -0300

    Delete module.d.ts

--- dist/module.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by cesar on 27/06/17.
- */
-import { ModuleWithProviders } from "@angular/core";
-export declare class HttpServiceModule {
-    static forRoot(config?: any): ModuleWithProviders;
-}
